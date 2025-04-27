8d62deb4c2c450994f5a6ca048be45e574cbe6b2
ItsClaudeee4 <164259725+ItsClaudeee4@users.noreply.github.com>
2025-04-27 22:08:23 +0100

    lalalalala

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ShopContextProvider from "./context/ShopContext.jsx";
+import { HashRouter } from "react-router-dom"; // Change to HashRouter
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
